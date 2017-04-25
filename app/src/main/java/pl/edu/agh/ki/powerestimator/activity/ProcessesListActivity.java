6fb007fd7ff034c14aed8e945a7edd18fadf4d36
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2017-04-25 21:07:05 +0200

    Fixed unresponsiveness

--- app/src/main/java/pl/edu/agh/ki/powerestimator/activity/ProcessesListActivity.java
+++ app/src/main/java/pl/edu/agh/ki/powerestimator/activity/ProcessesListActivity.java
@@ -1,12 +1,10 @@
 package pl.edu.agh.ki.powerestimator.activity;
 
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningAppProcessInfo;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -14,10 +12,10 @@ import android.widget.ListView;
 import com.jaredrummler.android.processes.AndroidProcesses;
 import com.jaredrummler.android.processes.models.AndroidAppProcess;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -28,28 +26,33 @@ import pl.edu.agh.ki.powerestimator.model.ProcessListItem;
 import pl.edu.agh.ki.powerestimator.model.ProcessListItemAdapter;
 
 public class ProcessesListActivity extends AppCompatActivity {
-
-    private ActivityManager activityManager;
     private final ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
     private ScheduledFuture<?> future;
-    private final List<ProcessListItem> items = new CopyOnWriteArrayList<>();
     private ProcessListItemAdapter adapter;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            List<ProcessListItem> newProcesses = msg.getData().getParcelableArrayList("processes");
+            adapter.changeProcesses(newProcesses);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_processes_list);
-        activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
 
         ListView processesListView = (ListView) findViewById(R.id.processesListView);
-        adapter = new ProcessListItemAdapter(getApplicationContext(), items);
+        adapter = new ProcessListItemAdapter(getApplicationContext());
         processesListView.setAdapter(adapter);
         processesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                final ProcessListItem item = items.get(position);
+                final ProcessListItem item = (ProcessListItem) adapter.getItem(position);
                 final Intent intent =
-                        new Intent(getApplicationContext(), ProcessInfoActivity.class);
+                        new Intent(ProcessesListActivity.this, ProcessInfoActivity.class);
                 intent.putExtra("pid", item.getPid());
                 intent.putExtra("uid", item.getUid());
                 intent.putExtra("name", item.getName());
@@ -59,35 +62,41 @@ public class ProcessesListActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
+        if (future != null && !future.isCancelled()) {
+            future.cancel(true);
+        }
         future = service.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
-                items.clear();
-                Log.i(ProcessesListActivity.class.getSimpleName(), "Refreshing processes list");
+                ArrayList<ProcessListItem> newProcesses = new ArrayList<>();
                 final List<AndroidAppProcess> processes = AndroidProcesses.getRunningAppProcesses();
                 for (AndroidAppProcess process : processes) {
                     final ProcessListItem item = new ProcessListItem(
                             process.uid, process.pid, process.name
                     );
-                    items.add(item);
+                    newProcesses.add(item);
                 }
-
-                Collections.sort(items, new Comparator<ProcessListItem>() {
+                Collections.sort(newProcesses, new Comparator<ProcessListItem>() {
                     @Override
                     public int compare(ProcessListItem o1, ProcessListItem o2) {
                         return o1.getName().compareTo(o2.getName());
                     }
                 });
-                adapter.notifyDataSetChanged();
+                Message message = new Message();
+                Bundle data = new Bundle();
+                data.putParcelableArrayList("processes", newProcesses);
+                message.setData(data);
+                handler.sendMessage(message);
             }
-        }, 10, 1000, TimeUnit.MILLISECONDS);
+        }, 100, 1000, TimeUnit.MILLISECONDS);
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         future.cancel(true);
+        future = null;
     }
 }
